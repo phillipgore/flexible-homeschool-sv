059d3940686e08f8c7533525c5699d3b9e5b5e19
Phillip Gore <phillipagore@me.com>
2025-06-20 16:57:42 -0500

    nav changes

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -12,7 +12,7 @@
 		<ul class="col-tn-1of2 col-xs-1of3 off-xs-1of6 col-md-1of4 off-md-1of4 col-lg-1of6 off-lg-2of6">
 			<li class="dis-tn-block"><a class="m-tn-t-0" href="{base}/">Home</a></li>
 			<li class="dis-tn-block"><a class="m-tn-t-0" href="{base}/application">App</a></li>
-            <li class="dis-tn-block"><a class="m-tn-t-0" href="{base}/tutorials">Tuts</a></li>
+            <li class="dis-tn-block"><a class="m-tn-t-0" href="{base}/tutorials">Tutorials</a></li>
 			<li class="dis-tn-block"><a class="m-tn-t-0" href="{base}/faq">FAQ</a></li>
 		</ul>
 		<ul class="col-tn-1of2 col-xs-1of3 col-md-1of4 col-lg-1of6">
